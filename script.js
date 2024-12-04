86da41bf6f11c98d6491ec247166cfeba917d406
nicolemaracaja <nicolebrito.maracaja@gmail.com>
2024-12-04 17:47:37 -0300

    commit por nicole

---
+++ script.js
@@ -0,0 +1,27 @@
+const output = document.getElementById("output")
+
+function mostrarTabuada(){
+    //output.innerHTML = "valor qualquer"
+
+    let n = prompt("Digite um número")
+
+    if (n.trim() === "" || isNaN(n)) {
+        alert("Digite um valor válido!")
+        return
+    }
+
+    n = parseFloat(n)
+
+    let table = "<table><tr><th>Operação</th><th>Resultado</th></tr>";
+
+    for (let i = 0; i <= 10; i++){
+        table += `<tr><td>${n} x ${i}</td><td>${n * i}</td></tr>`;
+    }
+
+    table += "</table>";
+    output.innerHTML = table;
+}
+
+function limparTabuada(){
+    output.innerHTML = ""
+}
